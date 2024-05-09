fd633489dfeb17a18512bb747b1c1945c0de1f35
wuzhenbin <1137057407@qq.com>
2024-05-09 19:34:54 +0800

    IT

--- test/Snapshots.bsc.sol
+++ test/Snapshots.bsc.sol
@@ -6,6 +6,7 @@ import "forge-std/Test.sol";
 import "@interface/IERC20.sol";
 import "@interface/IWBNB.sol";
 import "@interface/IPancakeV2.sol";
+import "@interface/IPancakeV3.sol";
 
 // token
 WBNB constant BNB = WBNB(0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c);
@@ -25,6 +26,10 @@ IPancakePairV2 constant USDT_BNB_V2_Pool = IPancakePairV2(
 );
 // pancakeswap v3
 address constant PANCAKE_V3_FACTORY = 0x0BFbCF9fa4f9C56B0F40a671Ad40E0805A091865;
+// USDT_USDC
+IPancakePairV3 constant USDT_USDC_Pool = IPancakePairV3(
+    0x36696169C63e42cd08ce11f5deeBbCeBae652050
+);
 bytes32 constant PancakeV3_POOL_INIT_CODE_HASH = 0x6ce8eb472fa82df5469c6ab6d485f17c3ad13c8cd7af59b3d4a8026c5ce0f7e2;
 
 contract MockTest is Test {
